8583c6a06ed8e858e5b554699c4c971cae2da4c8
Max Hawkins <maxhawkins@google.com>
2015-01-17 14:58:29 -0800

    lint code

--- GooglyEyeArduino/GooglyEyeArduino.ino
+++ GooglyEyeArduino/GooglyEyeArduino.ino
@@ -1,106 +1,87 @@
-//This is the main loop for the Googly Eyes.
-//It was written with reference to the code of several Instructablers. Randy, Amanda, and more.
+// This is the main loop for the Googly Eyes.
+// It was written with reference to the code of several Instructablers. Randy, Amanda, and more.
 
-//See the instructable at:
+// See the instructable at:
 
-#include <Firmata.h>//Easiest most reilable way to move values from computer to Arduino
-#include <AccelStepper.h> //A great absolute and non-blocking stepper contro library
-#include <Servo.h>//Used to send servo-type signals to the DC motor controller
+// #include <Firmata.h>  // Easiest most reilable way to move values from computer to Arduino
+#include <AccelStepper.h>  // A great absolute and non-blocking stepper contro library
+#include <Servo.h>  // Used to send servo-type signals to the DC motor controller
 
-AccelStepper stepper(2,12,13);//initialize a two-wire stepper on pins 12 and 13
-Servo rotationalMotorController;//initialize our "servo", actually a Parralx
+AccelStepper stepper(AccelStepper::FULL2WIRE, 12, 13);  // initialize a two-wire stepper on pins 12 and 13
+Servo rotationalMotorController;  // initialize our "servo", actually a Parralx
 
-const boolean isLeftEye = true;//The two eyes are different.
+const boolean isLeftEye = true;  // The two eyes are different.
 
-const int pwmA = 3;//On Motor Shield, PWM control pins
+const int pwmA = 3;  // On Motor Shield, PWM control pins
 const int pwmB = 11;
-const int brakeA = 8;//On Motor Shield, Brake Control pins
+const int brakeA = 8;  // On Motor Shield, Brake Control pins
 const int brakeB = 9;
 
-const int hiLimitPin = A5;//Photodiode linear limit switch pins. Active low.
+const int hiLimitPin = A5;  // Photodiode linear limit switch pins. Active low.
 const int centerLimitPin = A3;
 const int lowLimitPin = A4;
 
-const int stepperSpeed = 600;//trial and error detemrined this to be the best speed.
+const int stepperSpeed = 600;  // trial and error detemrined this to be the best speed.
 
-const int stepsPerRotation = 200;//Our stepper is a 1.8 degree per step motor
+const int stepsPerRotation = 200;  // Our stepper is a 1.8 degree per step motor
 
-volatile int hiLimitValue; //state of linear limit switch
-volatile int centerLimitValue; //state of linear limit switch
-volatile int lowLimitValue; //state of linear limit switch
+int hiLimitValue;  // state of linear limit switch
+int centerLimitValue;  // state of linear limit switch
+int lowLimitValue;  // state of linear limit switch
 
-volatile long hiLimitSteps = 1600; //maximum linear steps from origin we can reasonably expect before tripping a limit switch
-volatile long loLimitSteps = -1600;
+long hiLimitSteps = 1600;  // maximum linear steps from origin we can reasonably expect before tripping a limit switch
+long loLimitSteps = -1600;
 
-volatile int overUnderOn = -1;//linear actuator relation to origin. -1 under 0 on 1 over.
-// volatile int i = 0;
-volatile long linearStepperGoal =0;
+long linearStepperGoal = 0;
 
-volatile int rotaryPositionGoal = 6;
-volatile int rotaryPositionMillis;
-volatile int lastKnownSpinPosition = -1;//was 6 a moment ago
-volatile int lastKnownLateralPosition = 0;//always known!
+int rotaryPositionGoal = 6;
+int rotaryPositionMillis;
+int lastKnownSpinPosition = -1;  // was 6 a moment ago
+int lastKnownLateralPosition = 0;  // always known!
 
-void setup()
-{  
-  firmataFakeAnalogSetup();
+void setup() {
+  // firmataFakeAnalogSetup();
   linearSetup();
   linearHome();
   rotarySetup();
   rotaryHome();
 }
 
-void loop(){
-  if(!updateLinearLimits()){//returns false when limit is tripped
-    linearHome();//if the carriage hits a limit switch, it is not properly zeroed and should be zero'd again. 
+void loop() {
+  if (!updateLinearLimits()) {  // returns false when limit is tripped
+    linearHome();  // if the carriage hits a limit switch, it is not properly zeroed and should be zero'd again. 
   }
-  while(Firmata.available()) {//if it gets a command from firmata, it does what it is supposed to do
-    Firmata.processInput();
-    stepper.moveTo(linearStepperGoal);
-  }
-  //LINEAR
+  // while (Firmata.available()) {  // if it gets a command from firmata, it does what it is supposed to do
+  //   Firmata.processInput();
+  //   stepper.moveTo(linearStepperGoal);
+  // }
+  // LINEAR
+  stepper.moveTo(hiLimitSteps);
   stepper.setSpeed(stepperSpeed);
   stepper.runSpeedToPosition();
-  //ROTATIONAL
-  spinTo(rotaryPositionGoal);
+  // // ROTATIONAL
+  while(!spinTo(4)) {}
 }
 
 ///Firmata Stuff!
 
-void analogWriteCallback(byte pin, int value)
-{
+void analogWriteCallback(byte pin, int value) {
   int imaginaryPinUsedToRecieveScaledLinearGoal = 2;
   int imaginaryPinUsedToRecieveRotaryGoal = 3;
 
-  if(pin ==imaginaryPinUsedToRecieveScaledLinearGoal ){
-    linearStepperGoal=map(value,0,360,loLimitSteps, hiLimitSteps);
+  if (pin == imaginaryPinUsedToRecieveScaledLinearGoal) {
+    linearStepperGoal = map(value, 0, 360, loLimitSteps, hiLimitSteps);
   }
-  if(pin ==imaginaryPinUsedToRecieveRotaryGoal ){
-    rotaryPositionGoal = value; 
+  if (pin == imaginaryPinUsedToRecieveRotaryGoal) {
+    rotaryPositionGoal = value;
   }
 }
 
-void firmataFakeAnalogSetup() 
-{
-  Firmata.setFirmwareVersion(0, 2);
-  Firmata.attach(ANALOG_MESSAGE, analogWriteCallback);
-  Firmata.begin(57600);
-  Serial.println("\tFirmata Begun.");
-  int imaginaryPinUsedToDifferentiateTheEyes = 0;
-  Firmata.sendAnalog(imaginaryPinUsedToDifferentiateTheEyes, isLeftEye);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// void firmataFakeAnalogSetup() {
+//   Firmata.setFirmwareVersion(0, 2);
+//   Firmata.attach(ANALOG_MESSAGE, analogWriteCallback);
+//   Firmata.begin(57600);
+//   Serial.println("\tFirmata Begun.");
+//   int imaginaryPinUsedToDifferentiateTheEyes = 0;
+//   Firmata.sendAnalog(imaginaryPinUsedToDifferentiateTheEyes, isLeftEye);
+// }
